7d122f7adf7705b1f3873bb04818699fe53d3073
金泽坤 <zekun.jin@npi.org.cn>
2019-09-18 18:08:14 +0800

    edit user info

---
+++ src/views/Mine/Info/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <van-cell-group>
+      <van-field label="头像">
+        <template slot="input">
+          <van-uploader style="float: right; padding: 0; margin: 0;" :before-read="beforeRead" :after-read="afterRead" :preview-image="true">
+            <van-image round width="24px" height="24px" :src="ossImageUrl(userForm.avatar)" fit="cover"/>
+          </van-uploader>
+        </template>
+      </van-field>
+      <van-field label="用户名" input-align="right" v-model="userForm.name" placeholder="请输入用户名" />
+      <van-cell @click="showSex = true" title="性别" :value="userForm.sex" is-link/>
+      <van-cell @click="showBirth = true" title="生日" :value="birthText" is-link/>
+    </van-cell-group>
+
+    <van-cell-group style="margin-top: 10px;">
+      <van-field label="手机" input-align="right" v-model="userForm.phone" placeholder="请输入手机" disabled/>
+      <van-field label="邮箱" input-align="right" v-model="userForm.email" placeholder="请输入邮箱" />
+      <van-field label="QQ" input-align="right" v-model="userForm.qqInfo" placeholder="请输入QQ" />
+      <van-field label="微信" input-align="right" v-model="userForm.wxInfo" placeholder="请输入微信" />
+    </van-cell-group>
+
+    <van-button
+      type="primary"
+      @click="saveInfo()"
+      style="margin: 15px 0; width: 95vw; margin-left: 2.5vw"
+    >保存</van-button>
+
+    <van-popup v-model="showSex" position="bottom">
+      <van-picker :columns="columns" @change="onChange" />
+    </van-popup>
+
+    <van-popup v-model="showBirth" position="bottom">
+      <van-datetime-picker
+        v-model="birthday"
+        @confirm="confirmBirth()"
+        @cancel="showBirth = false"
+        type="date"
+        :max-date="today"
+        :min-date="minDate"
+      />
+    </van-popup>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { getUserInfo } from '@/api/user';
+import { updateAvatar, editUserInfo } from '@/api/mine';
+import moment from 'moment';
+import { Cell, CellGroup, Button, Image, Grid, GridItem, Uploader, Field, Picker, Popup, DatetimePicker } from 'vant';
+Vue.use(Cell).use(CellGroup).use(Button).use(Image).use(Grid).use(GridItem).use(Uploader).use(Field).use(Picker).use(Popup).use(DatetimePicker);
+
+@Component({
+  components: {}
+})
+export default class MineInfo extends Vue {
+  public userForm: any = {};
+  public birthText: string = '请选择生日';
+  public columns: any = ['保密', '男', '女'];
+  public showSex: boolean = false;
+  public showBirth: boolean = false;
+  public today: Date = new Date();
+  public birthday: Date = new Date();
+  public minDate: Date = new Date(this.today.getFullYear() - 50, this.today.getMonth(), this.today.getDate());
+
+  public created() {
+    this.fetchInfo();
+  }
+
+  private async fetchInfo() {
+    const vm: any = this;
+    vm.$toast.loading({
+      mask: true,
+      forbidClick: true,
+      message: "加载中..."
+    });
+    const res: any = await getUserInfo();
+    this.userForm = res.data.data;
+    if (!this.userForm.sex) {
+      this.userForm.sex = '保密';
+    }
+    if (this.userForm.birthday) {
+      this.birthText = this.userForm.birthday;
+    }
+    vm.$toast.clear();
+  }
+
+  private onChange(picker: any, value: any, index: any) {
+    this.userForm.sex = value;
+  }
+
+  private saveInfo() {
+    this.userForm.birthday = moment(this.birthday).format('YYYY-MM-DD');
+    editUserInfo(this.userForm).then(() => {
+      this.fetchInfo();
+    });
+  }
+
+  private beforeRead(file: any) {
+    if (file.type === 'image/jpeg' || file.type === 'image/png') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private confirmBirth() {
+    this.birthText = moment(this.birthday).format('YYYY-MM-DD');
+    this.showBirth = false;
+  }
+
+  private async afterRead(file: any) {
+    const data = new FormData();
+    data.append('file', file.file);
+    const res = await updateAvatar(data);
+    const url = res.data.data.url;
+    await editUserInfo({
+      avatar: url,
+      id: this.userForm.id
+    });
+    this.fetchInfo();
+  }
+}
+</script>
+
+<style>
+.user-name {
+  position: absolute;
+  top: 50%;
+  transform: translate(15px, -40%);
+  font-size: 150%;
+}
+</style>
--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -19,7 +19,7 @@
     </van-grid>
 
     <van-cell-group style="margin-top: 10px;">
-      <van-cell title="我的名片" is-link/>
+      <van-cell title="我的名片" to="/mine/info" is-link/>
       <van-cell title="我的订单" to="/mine/order" is-link/>
       <van-cell title="我的活动" is-link />
       <van-cell title="我的机构" is-link />
