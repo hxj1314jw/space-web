52b22e22e0626c79d81a4f8d2bf455ab256d1941
金泽坤 <zekun.jin@npi.org.cn>
2019-09-23 10:24:57 +0800

    zone qualify

--- src/views/Home/Introduce/index.vue
+++ src/views/Home/Introduce/index.vue
@@ -6,18 +6,38 @@
         <van-cell-group style="margin-top: 0px; ">
           <van-image style="margin: 0; padding: 0" width="100%" height="230" fit="cover" :src="ossImageUrl(ZoneForm.imageUrl)" />
 
-          <van-cell title="联系电话: " :value="ZoneForm.contactInformation"></van-cell>
+          <van-cell title="联系电话: " :value="ZoneForm.contactInformation">
+            <van-icon slot="icon" name="phone-o" style="line-height: inherit; margin-right: 7px;" color="#00B261"/>
+          </van-cell>
       
-          <van-cell title="地理位置: " :value="ZoneForm.address"></van-cell>
+          <van-cell title="地理位置: " :value="ZoneForm.address">
+            <van-icon slot="icon" name="location-o" style="line-height: inherit; margin-right: 7px;" color="#00B261"/>
+          </van-cell>
+
+          <van-cell title="开放日: ">
+            <van-icon slot="icon" name="calender-o" style="line-height: inherit; margin-right: 7px;" color="#00B261"/>
+            <template v-for="(able, index) in ableList">
+              <span :key="index" style="background-color: #F76C6C; padding: 1px 3px; margin-left: 3px; border-radius: 3px;">
+                <span style="color: white; font-weight: bold; font-size: xx-small">{{ able }}</span>
+              </span>
+            </template>
+          </van-cell>
 
           <van-cell title="开放时间: ">
+            <van-icon slot="icon" name="clock-o" style="line-height: inherit; margin-right: 7px;" color="#00B261"/>
             <template>
               {{ ZoneForm.openDate }}
             </template>
           </van-cell>
+
+          <van-cell title="资质" is-link @click="showQualify()">
+            <van-icon slot="icon" name="orders-o" style="line-height: inherit; margin-right: 7px;" color="#00B261"/>
+            <template>
+              查看空间资质
+            </template>
+          </van-cell>
         </van-cell-group>
 
-        <van-cell title="资质" is-link @click="showQualify()"/>
         <van-cell-group style="margin-top: 10px; ">
           <van-cell>
             <span style="font-weight: bold">空间简介</span><br>
@@ -53,10 +73,41 @@
   export default class Aboutus extends Vue {
     public Brand: any = {};
     public ZoneForm: any = {};
+    public ableList: any = [];
     public activeName: string = 'zone';
 
     public created() {
       this.fetchZone();
+    }
+
+    private initAbleList() {
+      this.ableList = [];
+      const openDayList = this.ZoneForm.openDay.split(',');
+      for (const item of openDayList) {
+        switch (item) {
+          case '1':
+            this.ableList.push('一');
+            break;
+          case '2':
+            this.ableList.push('二');
+            break;
+          case '3':
+            this.ableList.push('三');
+            break;
+          case '4':
+            this.ableList.push('四');
+            break;
+          case '5':
+            this.ableList.push('五');
+            break;
+          case '6':
+            this.ableList.push('六');
+            break;
+          case '0':
+            this.ableList.push('日');
+            break;
+        }
+      }
     }
 
     private fetchBrand() {
@@ -72,8 +123,14 @@
     }
 
     private fetchZone() {
+      Toast.loading({
+        mask: true,
+        forbidClick: true,
+        message: '加载中..s.'
+      });
       getZoneDetail({id: this.$route.params.id}).then((res: any) => {
         this.ZoneForm = res.data.data;
+        this.initAbleList();
         Toast.clear();
       });
     }
