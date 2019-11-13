5ee024d690e48e6da8d57a75e488eef3c4c9f543
liuyumeng <476590393@qq.com>
2019-11-13 19:18:55 +0800

    问卷组件 问卷详情 问卷列表

---
+++ src/components/SurveyCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <van-cell-group style="margin-top: 10px;">
+    <van-cell>
+      <span>
+        {{ surveyForm }}
+        <van-tag color="rgba(193,255,182,.31)" text-color="#07c160" >活动1</van-tag>
+      </span>
+    </van-cell>
+  </van-cell-group>
+</template>
+
+
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Cell, CellGroup, Button, Tag, Grid, GridItem, Uploader } from 'vant';
+Vue.use(Cell).use(CellGroup).use(Button).use(Tag).use(Grid).use(GridItem).use(Uploader);
+
+@Component
+export default class SurveyCard extends Vue {
+  @Prop() public surveyForm!: any;
+
+
+
+}
+
+</script>
+
+
+<style>
+  .org-name {
+    position: absolute;
+    top: 50%;
+  }
+</style>
+
---
+++ src/views/Mine/Survey/Detail/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span>哈哈哈</span>
+    <div>
+      <van-radio-group v-model="radio">
+        <van-radio name="1">单选框 1</van-radio>
+        <van-radio name="2">单选框 2</van-radio>
+      </van-radio-group>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+  import { Component, Vue, Prop } from "vue-property-decorator";
+  import { RadioGroup, Radio } from 'vant';
+  Vue.use(RadioGroup).use(Radio);
+
+  export default class SurveyDetail extends Vue {
+    public radio: string = '1';
+    public questList: any = [];
+
+
+
+  }
+</script>
+
+
+
+<style>
+
+</style>
